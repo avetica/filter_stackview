743c6abb9ce35fa7a50b9f199e182e6bb711c940
Hamza Tamyachte <hamza.tamyachte@gmail.com>
2022-10-25 16:13:40 +0200

    WIP

--- classes/output/output_stackviewer.php
+++ classes/output/output_stackviewer.php
@@ -32,7 +32,7 @@ use renderer_base;
 use templatable;
 
 /**
- * Class stackview_output
+ * Class stack viewer output.
  *
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  *
